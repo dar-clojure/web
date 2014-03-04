9e0a10fd5ee930b685b741eba4fa9ac681c8f4c4
Eldar Gabdullin <eldargab@gmail.com>
2014-03-04 14:10:40 +0400

    simplify example

--- example/hello_world.clj
+++ example/hello_world.clj
@@ -1,7 +1,8 @@
 (require '[easy-app.web :as web :refer [Get send]])
 (require '[easy-app.web.server.jetty :as jetty])
 
-(Get "/" :fn (fn [] (send "Hello world!")))
+(Get "/"
+  :fn #(send "Hello world!"))
 
 (Get "/{greeting}"
   :args [:http-req]
